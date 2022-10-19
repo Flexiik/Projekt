3c14e9a1269347d48a59e95fef8abcae665c03d0
Flexiik <57681244+Flexiik@users.noreply.github.com>
2022-10-19 18:42:22 +0200

    Day Two

--- scripts.js
+++ scripts.js
@@ -1,28 +1,29 @@
 function vypocitejCtverec() {
-    let x = document.getElementById("strana-a_c").value;
-    let text;
-    if(isNaN(x) || x <= 0) {
-        text = "Nevalidni input";
-    } else {
-        text = "Povrch: " + x*x + "cm&sup2<br>Obvod: " + x*4 + "cm<br>Úhlopříčka: " + (Math.sqrt(x*x + x*x)).toFixed(2) + "cm";
-    }
-    document.getElementById("ccv").innerHTML = text;
+  let x = document.getElementById("strana-a_c").value;
+  let text;
+  if (isNaN(x) || x <= 0) {
+    text = "Nevalidni input";
+  } else {
+    text = "Povrch: " + x * x + "cm&sup2<br>Obvod: " + x * 4 + "cm<br>Úhlopříčka: " + (Math.sqrt(x * x + x * x)).toFixed(2) + "cm";
+  }
+  document.getElementById("ccv").innerHTML = text;
 }
 
 function vypocitejObdelnik() {
-    let x = document.getElementById("strana-a_o").value;
-    let y = document.getElementById("strana-b_o").value;
-    let text;
-    if(isNaN(x) || x <= 0 || isNaN(y) || y <= 0) {
-        text = "Nevalidni input";
-    } else {
-        text = "Povrch: " + x*y + "cm&sup2<br>Obvod: " + ((x*2)+(y*2)) + "cm<br>Uhlopricka: " + Math.sqrt(x^2 + y^2).toFixed(2) + "cm";
-    }
-    document.getElementById("oov").innerHTML = text;
+  let x = document.getElementById("strana-a_o").value;
+  let y = document.getElementById("strana-b_o").value;
+  let text;
+  if (isNaN(x) || x <= 0 || isNaN(y) || y <= 0) {
+    text = "Nevalidni input";
+  } else {
+    text = "Povrch: " + x * y + "cm&sup2<br>Obvod: " + ((x * 2) + (y * 2)) + "cm<br>Uhlopricka: " + Math.sqrt(x ^ 2 + y ^ 2).toFixed(2) + "cm";
+  }
+  document.getElementById("oov").innerHTML = text;
 }
 
 
 //trojuhelnik
+
 //lichobeznik
 //rovnobeznik
 //kruh
@@ -38,3 +39,9 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
