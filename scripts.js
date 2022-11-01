882aeec3de81717e1e523b3b2d5b45fd5dfef889
MatheX <46656456+mathexresources@users.noreply.github.com>
2022-11-01 12:10:59 +0100

    update localu

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function vypocitejCtverec() {
   let x = document.getElementById("strana-a_c").value;
   let text;
   if (isNaN(x) || x <= 0) {
-    text = "Nevalidni input";
+    text = "Neplatné hodnoty";
   } else {
     text = "Povrch: " + x * x + "cm&sup2<br>Obvod: " + x * 4 + "cm<br>Úhlopříčka: " + (Math.sqrt(x * x + x * x)).toFixed(2) + "cm";
   }
@@ -14,7 +14,7 @@ function vypocitejObdelnik() {
   let y = document.getElementById("strana-b_o").value;
   let text;
   if (isNaN(x) || x <= 0 || isNaN(y) || y <= 0) {
-    text = "Nevalidni input";
+    text = "Neplatné hodnoty";
   } else {
     text = "Povrch: " + x * y + "cm&sup2<br>Obvod: " + ((x * 2) + (y * 2)) + "cm<br>Uhlopricka: " + Math.sqrt(x * x + y * y).toFixed(2) + "cm";
   }
@@ -33,7 +33,7 @@ function vypocitejTrojuhelnik() {
 
   let text;
   if (isNaN(a) || a <= 0 || isNaN(va) || va <= 0) {
-    text = "Nevalidni input";
+    text = "Neplatné hodnoty";
   } else {
     text = "Obsah: " + obsah + "cm&sup2"
   }
@@ -56,7 +56,7 @@ function vypocitejLichobeznik() {
 
   let text;
   if (isNaN(a) || isNaN(b) || isNaN(c) || isNaN(d) || a <= 0 || b <= 0 || c <= 0 || d <= 0) {
-    text = "Nevalidni input";
+    text = "Neplatné hodnoty";
   } else {
     text = "Obvod: " + obvod + "cm<br>Obsah: " + obsah + "cm&sup2";
   }
@@ -69,7 +69,7 @@ function vypocitejRovnobeznik() {
   let h = document.getElementById("vyska_r").value;
   let text;
   if (isNaN(a) || a <= 0 || isNaN(h) || h <= 0) {
-    text = "Nevalidni input";
+    text = "Neplatné hodnoty";
   } else {
     text = "Povrch: " + (a*h).toFixed(2) + "cm&sup2";
   }
@@ -82,7 +82,7 @@ function vypocitejKruh() {
   let r = document.getElementById("strana-a_k").value;
   let text;
   if (isNaN(r) || r <= 0) {
-    text = "Nevalidni input";
+    text = "Neplatné hodnoty";
   } else {
     text = "Povrch: " + (Math.PI * (r * r)).toFixed(2) + "cm&sup2<br>Obvod: " + (2 * Math.PI * r).toFixed(2) + "cm";
   }
